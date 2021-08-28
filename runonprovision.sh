2b10042d343fb551602d99aabdd5913839a6ef61
computerwarriorsits <75262445+computerwarriorsits@users.noreply.github.com>
2021-08-27 22:30:49 -0400

    Create runonprovision.sh

---
+++ runonprovision.sh
@@ -0,0 +1,20 @@
+#!/bin/vbash
+#delete runonprovision task
+
+readonly logFile="/var/log/runonprovision.log"
+
+source /opt/vyatta/etc/functions/script-template
+
+configure > ${logFile}
+delete system task-scheduler task runonprovision  >> ${logFile}
+commit >> ${logFile}
+save >> ${logFile}
+#exit
+
+#download reboot script. cannot create task schedule before file exists
+/usr/bin/curl --silent 'https://raw.githubusercontent.com/computerwarriorsits/UnifiUSGReboot/reboot.sh' > /config/scripts/reboot.sh
+/bin/chmod +x /config/scripts/reboot.sh
+
+#add task to run every day at 3am
+touch /var/spool/cron/crontabs/root
+{ crontab -l; echo "0 3 * * 7 /config/scripts/restartvpn.sh"; } | crontab -
